5d210aa67d74b9e64eaa6f769d318a1bb83ee413
Johannes Filter <hi@jfilter.de>
2020-04-27 23:17:41 +0200

    add script to normalize files

---
+++ utils/normalize_files.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -e
+set -x
+
+# set correct file permissions https://stackoverflow.com/a/28993800/4028896
+# normalize filenames with detox https://github.com/dharple/detox
+
+# only work the absulute path
+if [[ $1 == /* ]]; then
+  input=$1
+else
+  input=$PWD/$1
+fi
+
+detox $input
+find $input -type f -print0 | xargs -0 chmod 0664
+find $input -type d -print0 | xargs -0 chmod 0775
