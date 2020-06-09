aef2a1bad3ac42f41509d260cf5763aab95e8dfe
Johannes Filter <hi@jfilter.de>
2020-06-09 23:18:46 +0200

    add setup script

---
+++ setup.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -e
+set -x
+
+# post-installation: run `parallel --citation`
+
+command_exists() {
+  if ! [ -x $($(command -v $1 &>/dev/null)) ]; then
+    echo $(error: $1 is not installed.) >&2
+    exit 1
+  fi
+}
+
+# for macos
+if [ "$(uname)" == "Darwin" ]; then
+  command_exists brew
+  command_exists docker
+
+  brew install parallel ghostscript mupdf-tools qpdf poppler detox exiftool
+fi
+
+# for centos
+if [ -f /etc/redhat-release ]; then
+  echo "not yet implemented"
+  exit 1
+fi
+
+# for ubuntu
+if [ -f /etc/lsb-release ]; then
+  # not sure, TODO
+  apt-get update && apt-get install -y parallel ghostscript mupdf-tools qpdf poppler-utils detox libimage-exiftool-perl docker.io
+fi
